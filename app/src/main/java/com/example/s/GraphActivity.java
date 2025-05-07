e3a120e5d3efd317338021d43b8155f46f5ae069
Alkata <aleksnedkov69@gmail.com>
2025-05-07 20:09:51 +0200

    add checkboxes for health data visibility in GraphActivity; update layout and improve graph setup

--- app/src/main/java/com/example/s/GraphActivity.java
+++ app/src/main/java/com/example/s/GraphActivity.java
@@ -2,24 +2,20 @@ package com.example.s;
 
 import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.activity.ComponentActivity;
-import androidx.annotation.NonNull;
-
+import android.widget.CheckBox;
+import androidx.appcompat.app.AppCompatActivity;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import android.content.SharedPreferences;
-import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class GraphActivity extends ComponentActivity {
+public class GraphActivity extends AppCompatActivity {
 
     private LineChart lineChart;
+    private LineDataSet heartRateDataSet, saturationDataSet, temperatureDataSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +24,21 @@ public class GraphActivity extends ComponentActivity {
 
         lineChart = findViewById(R.id.line_chart);
 
+        CheckBox heartRateCheckBox = findViewById(R.id.checkbox_heart_rate);
+        CheckBox saturationCheckBox = findViewById(R.id.checkbox_saturation);
+        CheckBox temperatureCheckBox = findViewById(R.id.checkbox_temperature);
+
         // Get health data from intent
         List<String[]> healthDataList = (List<String[]>) getIntent().getSerializableExtra("HEALTH_DATA");
-        Log.d("GraphActivity", "Received health data: " + healthDataList);
 
         if (healthDataList != null) {
             setupGraph(healthDataList);
         }
-    }
 
-    private String loadHealthData() {
-        SharedPreferences prefs = getSharedPreferences("HealthDataPrefs", MODE_PRIVATE);
-        return prefs.getString("LastHealthData", "No health data available");
+        // Set listeners for checkboxes
+        heartRateCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> toggleLineVisibility(heartRateDataSet, isChecked));
+        saturationCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> toggleLineVisibility(saturationDataSet, isChecked));
+        temperatureCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> toggleLineVisibility(temperatureDataSet, isChecked));
     }
 
     private void setupGraph(List<String[]> healthDataList) {
@@ -58,7 +57,25 @@ public class GraphActivity extends ComponentActivity {
             temperatureEntries.add(new Entry(i, temperature));
         }
 
-        LineData lineData = getLineData(heartRateEntries, saturationEntries, temperatureEntries);
+        heartRateDataSet = new LineDataSet(heartRateEntries, "Heart Rate");
+        heartRateDataSet.setColor(Color.RED);
+        heartRateDataSet.setLineWidth(2f);
+        heartRateDataSet.setCircleColor(Color.RED);
+        heartRateDataSet.setCircleRadius(4f);
+
+        saturationDataSet = new LineDataSet(saturationEntries, "Saturation");
+        saturationDataSet.setColor(Color.BLUE);
+        saturationDataSet.setLineWidth(2f);
+        saturationDataSet.setCircleColor(Color.BLUE);
+        saturationDataSet.setCircleRadius(4f);
+
+        temperatureDataSet = new LineDataSet(temperatureEntries, "Temperature");
+        temperatureDataSet.setColor(Color.GREEN);
+        temperatureDataSet.setLineWidth(2f);
+        temperatureDataSet.setCircleColor(Color.GREEN);
+        temperatureDataSet.setCircleRadius(4f);
+
+        LineData lineData = new LineData(heartRateDataSet, saturationDataSet, temperatureDataSet);
         lineChart.setData(lineData);
 
         lineChart.setBackgroundColor(Color.WHITE);
@@ -79,27 +96,8 @@ public class GraphActivity extends ComponentActivity {
         lineChart.invalidate();
     }
 
-    @NonNull
-    private static LineData getLineData(List<Entry> heartRateEntries, List<Entry> saturationEntries, List<Entry> temperatureEntries) {
-        LineDataSet heartRateDataSet = new LineDataSet(heartRateEntries, "Heart Rate");
-        heartRateDataSet.setColor(Color.RED);
-        heartRateDataSet.setLineWidth(2f);
-        heartRateDataSet.setCircleColor(Color.RED);
-        heartRateDataSet.setCircleRadius(4f);
-
-        LineDataSet saturationDataSet = new LineDataSet(saturationEntries, "Saturation");
-        saturationDataSet.setColor(Color.BLUE);
-        saturationDataSet.setLineWidth(2f);
-        saturationDataSet.setCircleColor(Color.BLUE);
-        saturationDataSet.setCircleRadius(4f);
-
-        LineDataSet temperatureDataSet = new LineDataSet(temperatureEntries, "Temperature");
-        temperatureDataSet.setColor(Color.GREEN);
-        temperatureDataSet.setLineWidth(2f);
-        temperatureDataSet.setCircleColor(Color.GREEN);
-        temperatureDataSet.setCircleRadius(4f);
-
-        LineData lineData = new LineData(heartRateDataSet, saturationDataSet, temperatureDataSet);
-        return lineData;
+    private void toggleLineVisibility(LineDataSet dataSet, boolean isVisible) {
+        dataSet.setVisible(isVisible);
+        lineChart.invalidate();
     }
 }
